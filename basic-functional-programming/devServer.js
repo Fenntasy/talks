28fb233f6803bcf14b51724d5d10e6f83600c2a2
Vincent Billey <vbilley@axiome.io>
2015-11-09 13:59:31 +0100

    basic functional programming : first slides

--- basic-functional-programming/devServer.js
+++ basic-functional-programming/devServer.js
@@ -13,15 +13,19 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
+app.get('/img/:file', function(req, res) {
+    res.sendFile(path.join(__dirname, 'img/' + req.params.file ))
+})
+
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(3001, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:3001');
 });
--- basic-functional-programming/src/deck.js
+++ basic-functional-programming/src/deck.js
@@ -6,11 +6,30 @@ export default class MyDeck extends Component {
   render() {
     return (
       <Deck>
-        <Slide style={{background: 'yellow'}}>
-          slide 1
+        <Slide>
+          <h1>Introduction à la programmation fonctionnelle</h1>
+          <h2>en javascript (si si)</h2>
         </Slide>
-        <Slide style={{background: 'green'}}>2</Slide>
-        <Slide style={{background: 'red'}}>3</Slide>
+        <Slide>
+            <h1>Vincent Billey aka Fenn</h1>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div><img src="/img/axiome.png" /></div>
+                <div><img src="/img/metod.png" /></div>
+            </div>
+        </Slide>
+        <Slide>
+            <div className="title">Pourquoi javascript ?</div>
+            <q>Imitation is the sincerest form of flattery</q>
+        </Slide>
+        <Slide>
+            <a className="title" href="http://www.w3.org/TR/html-design-principles/">HTML Design Principles - 3.2. Priority of Constituencies</a>
+            <q style={{fontSize: '1.2rem', textAlign: 'left'}}>
+                In case of conflict, <b>consider users over authors over implementors over specifiers over theoretical purity</b>.
+                In other words costs or difficulties to the user should be given more weight than costs to authors;
+                which in turn should be given more weight than costs to implementors; which should be given more weight than costs to authors
+                of the spec itself, which should be given more weight than those proposing changes for theoretical reasons alone.
+                Of course, it is preferred to make things better for multiple constituencies at once.</q>
+        </Slide>
       </Deck>
     )
   }
